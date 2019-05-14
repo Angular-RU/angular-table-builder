2d75dcaa39df023105dc14e73431d7433c654e12
m.ivanov4 <m.ivanov4@tinkoff.ru>
2019-05-14 10:46:49 +0300

    feat: add toolbar

--- src/app/samples/sample-first/sample-first.module.ts
+++ src/app/samples/sample-first/sample-first.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SampleFirstComponent } from './sample-first.component';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../../shared/material/material.module';
 
 @NgModule({
     declarations: [SampleFirstComponent],
-    imports: [CommonModule, RouterModule.forChild([{ path: '', component: SampleFirstComponent }])]
+    imports: [CommonModule, MaterialModule, RouterModule.forChild([{ path: '', component: SampleFirstComponent }])]
 })
 export class SampleFirstModule {}
