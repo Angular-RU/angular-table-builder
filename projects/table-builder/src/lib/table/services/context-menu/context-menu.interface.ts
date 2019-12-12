acb5479cac2da5d5f2193307e00928730d553b73
m.ivanov4 <Splincode1994>
2019-12-12 12:40:25 +0300

    small fix

--- projects/table-builder/src/lib/table/services/context-menu/context-menu.interface.ts
+++ projects/table-builder/src/lib/table/services/context-menu/context-menu.interface.ts
@@ -1,11 +1,10 @@
-import { TableRow } from '../../interfaces/table-builder.external';
 import { Any, MousePosition } from '../../interfaces/table-builder.internal';
 
 export class ContextMenuState {
     public opened: boolean = false;
     public position: MousePosition = { left: null, top: null };
     public key: string = null;
-    public item: TableRow = null;
+    public item: Any = null;
     public value: Any = null;
 
     constructor(state: Partial<ContextMenuState> = null) {
